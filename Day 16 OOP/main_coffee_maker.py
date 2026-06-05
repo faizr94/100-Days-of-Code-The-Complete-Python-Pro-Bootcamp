2805bf7ccd73fbce128652da85d38d6d100b6ee2
Admin <faizrusydi094@gmail.com>
2026-06-06 00:58:09 +0800

    coffee maker program

--- Day 16 OOP/main_coffee_maker.py
+++ Day 16 OOP/main_coffee_maker.py
@@ -2,15 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# We get the items on menu first 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
 
-coffee_menu = Menu()
-drinks_available = coffee_menu.get_items()
-
-print(drinks_available)
-
-x = coffee_menu.find_drink('espresso')
-print(x.name)
-print(x.ingredients)
-print(x.cost)
-
-
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}:  ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        resource_report = coffee_maker.report()
+        cash_report = money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            print(f"{choice.title()} costs ${drink.cost}.")
+            transaction_success = money_machine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
+ 
