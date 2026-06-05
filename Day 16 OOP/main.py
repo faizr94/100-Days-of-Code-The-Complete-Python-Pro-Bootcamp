1a4c73e247d7ca3b421e259129b4ba5bd6d9759d
Admin <faizrusydi094@gmail.com>
2026-06-05 20:39:59 +0800

    practicing calling methods

--- Day 16 OOP/main.py
+++ Day 16 OOP/main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from 100-days-of-code-the-complete-python-pro-bootcamp!")
+# object.attribute
+# object.method
+import prettytable
+from prettytable import PrettyTable
+import turtle
+from turtle import Turtle, Screen
+
+'''
+Timmy Section, just trying out different attributes and methods
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('coral')
+timmy.forward(100)
 
 
-if __name__ == "__main__":
-    main()
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+'''
+
+# Using prettytable to create ASCII tables
+table = PrettyTable()
+table.add_column(
+    "Pokemon Name",
+    ["Pikachu", "Squirtle", "Charmander"],
+    align='l'
+)
+table.add_column(
+    "Type",
+    ["Electric", "Water", "Fire"]
+)
+
+print(table)
